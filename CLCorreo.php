051e6ce09aafea8c6097b7cf8a1925bb22c31b60
FABRIZIO ESTHEFANO JURADO CÁRDENAS <1473528@senati.pe>
2024-09-28 16:23:37 -0500

    Ultimos ajustes de archivo CLCorreo.php

--- CLCorreo.php
+++ CLCorreo.php
@@ -1,12 +1,11 @@
 <?php
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
-use Dotenv\Dotenv;  // Importar la clase Dotenv para cargar el archivo .env
-
+ 
 require 'vendor/autoload.php';
 
 // Cargamos las variables del archivo credenciales.env
-$dotenv = Dotenv::createImmutable(__DIR__, 'credenciales.env'); 
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__, 'credenciales.env');
 $dotenv->load();
 
 $mail = new PHPMailer(true);
